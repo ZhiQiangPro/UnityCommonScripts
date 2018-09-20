3c1b1f2ac36a7a38be85efadfe1f63fb597a412e
ZhiQiangPro <39686392+ZhiQiangPro@users.noreply.github.com>
2018-09-20 15:04:16 +0800

    画线

---
+++ DrawTest.cs
@@ -0,0 +1,72 @@
+﻿/************************************************************
+  Copyright (C), 2007-2017,BJ Rainier Tech. Co., Ltd.
+  FileName: DrawTest.cs
+  Author:       Version :1.0          Date: 
+  Description:
+************************************************************/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.UI;
+
+public class DrawTest : MonoBehaviour
+{
+    private Transform beiJing;
+    private Camera UIcamera;
+    [SerializeField] private List<Vector2> pointList = new List<Vector2>();
+    [SerializeField] private List<int> breakpointList = new List<int>();
+
+
+
+    private List<Button> btnList = new List<Button>();
+
+    private void Awake()
+    {
+        beiJing = transform.Find("背景");
+        UIcamera = transform.Find("Camera").GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < beiJing.childCount; i++)
+        {
+            var i1 = i;
+            beiJing.GetChild(i).GetComponent<Button>().OnClickAsObservable().Subscribe(_ =>
+            {
+                if (!MyDrawGLine.canDraw)
+                {
+                    pointList.Add(UIcamera.WorldToScreenPoint(beiJing.GetChild(i1).position));
+                              
+                }
+
+                MyDrawGLine.canDraw = true;
+                pointList.Add(UIcamera.WorldToScreenPoint(beiJing.GetChild(i1).position));
+
+                MyDrawGLine.PointList = pointList;
+            });
+        }
+
+
+        this.UpdateAsObservable().Select(_ => Input.GetMouseButtonDown(1))
+            .Where(_ => _)
+            .Subscribe(_ =>
+            {
+                breakpointList.Add(pointList.Count-1);
+                MyDrawGLine.BreakpointList = breakpointList;
+            });
+    }
+
+
+    private void OnGUI()
+    {
+        /*if (GUI.Button(new Rect(0, 0, 75, 30), "确定"))
+        {
+           
+        }*/
+
+
+    }
+}
---
+++ MyDrawGLine.cs
@@ -0,0 +1,90 @@
+﻿/************************************************************
+  Copyright (C), 2007-2017,BJ Rainier Tech. Co., Ltd.
+  FileName: MyDrawGLine.cs
+  Author: 崔志强      Version :1.0          Date: 
+  Description:
+************************************************************/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MyDrawGLine : MonoBehaviour
+{
+    public Material mat;
+    public static bool canDraw = false;
+    private static List<Vector2> pointList;
+
+    public static List<Vector2> PointList
+    {
+        get { return pointList; }
+        set { pointList = value; }
+    }
+
+    private static List<int> breakpointList;
+
+    public static List<int> BreakpointList
+    {
+        get { return breakpointList; }
+        set { breakpointList = value; }
+    }
+
+  
+    private void Awake()
+    {
+        pointList = new List<Vector2>();
+        breakpointList = new List<int>();
+    }
+
+    /// <summary>
+    /// 开始画线
+    /// </summary>
+    public static void Draw()
+    {
+        canDraw = true;
+    }
+
+    /// <summary>
+    /// 停止画线
+    /// </summary>
+    public static void DontDraw()
+    {
+        canDraw = false;
+    }
+
+    void OnPostRender()
+    {
+        if (canDraw)
+        {
+            GL.PushMatrix();
+            mat.SetPass(0);
+            GL.LoadPixelMatrix();
+            GL.Color(Color.red);
+
+            for (int i = 0; i < pointList.Count - 1; i++)
+
+            {
+                for (int j = 0; j < breakpointList.Count; j++)
+
+                {
+                    if (i == (int) (breakpointList[j]))
+
+                    {
+                        i++;
+                    }
+                }
+
+                GL.Begin(GL.LINES);
+
+
+                GL.Vertex3((pointList[i]).x, pointList[i].y, 0);
+
+                GL.Vertex3((pointList[i + 1]).x, pointList[i + 1].y, 0);
+
+                GL.End();
+            }
+
+            GL.PopMatrix();
+        }
+    }
+}
